5125ed3ff39669db06341f66bdc00571786f955a
Teddy312 <50252116+Teddy312@users.noreply.github.com>
2019-05-05 12:37:23 +0200

    Uploaded source code

---
+++ sourcecode/game.lua
@@ -0,0 +1,118 @@
+-- Game States
+GameStates = {pause = 'pause', running = 'running', game_over = 'game over'}
+state = GameStates.running
+
+-- Start Position Snake
+local snakeX = 15
+local snakeY = 15
+local dirX = 0
+local dirY = 0
+
+local SIZE = 20
+local foodX = 0
+local foodY = 0
+local tail = {}
+
+tail_length = 0
+up = false
+down = false
+left = false
+right = false
+
+-- Add Apples at random points
+function add_food()
+  math.randomseed(os.time())
+  foodX = math.random(SIZE + 43)
+  foodY = math.random(SIZE + 15)
+  
+  
+end
+
+-- Draw Stuff In-game
+function game_draw()
+  -- Draw Snake Head
+	love.graphics.setColor(1, 1, 1, 1.0)
+	love.graphics.rectangle("fill", snakeX * SIZE, snakeY * SIZE, SIZE, SIZE)
+  
+  -- Draw Snake Tail
+  love.graphics.setColor(1, 1, 1, 0.7)
+  for _, v in ipairs(tail) do
+    love.graphics.rectangle("fill", v[1] * SIZE, v[2] * SIZE, SIZE, SIZE)
+  end
+  
+  -- Draw Apples
+  love.graphics.setColor(0.7, 0.35, 0.4, 1.0)
+  love.graphics.rectangle("fill", foodX * SIZE, foodY * SIZE, SIZE, SIZE)
+  
+  -- Draw Score Text
+  love.graphics.setColor(1, 1, 1, 1)
+  love.graphics.print('Score: '.. tail_length, 10, 10, 0, 1.5, 1.5, 0, 0, 0, 0)
+end
+
+-- Update Stuff In-Game
+function game_update()
+	-- Update Movement
+	if up and dirY == 0 then
+		dirX, dirY = 0, -1
+	elseif down and dirY == 0 then
+		dirX, dirY = 0, 1
+	elseif left and dirX == 0 then
+		dirX, dirY = -1, 0
+	elseif right and dirX == 0 then
+		dirX, dirY = 1, 0	
+	end
+  
+  local oldX = snakeX
+  local oldY = snakeY
+  
+  snakeX = snakeX + dirX
+	snakeY = snakeY + dirY
+  
+  -- Spawn new food when snake reaches it
+  if snakeX == foodX and snakeY == foodY then
+    add_food()
+    tail_length = tail_length + 1;
+    table.insert(tail, {0, 0})
+  end
+  
+  -- Check Left side Screen for Collision
+  if snakeX < 0 then
+    state = GameStates.game_over
+  -- Check Right side Screen for Collision
+  elseif snakeX > SIZE + 43 then
+    state = GameStates.game_over
+  -- Check Up side Screen for Collision
+  elseif snakeY < 0 then
+    state = GameStates.game_over
+  -- Check Down side Screen for Collision
+  elseif snakeY > SIZE + 15 then
+    state = GameStates.game_over
+  end
+  
+  -- Add tail behind snake head
+  if tail_length > 0 then
+    for _, v in ipairs(tail) do
+      local x, y = v[1], v[2]
+      v[1], v[2] = oldX, oldY
+      oldX, oldY = x, y
+    end
+  end
+  
+  -- Checks if snake collids with itself
+  for _, v in ipairs(tail) do 
+    if snakeX == v[1] and snakeY == v[2] then
+      state = GameStates.game_over
+    end
+  end
+end
+
+-- Resets State of the Game when Restarting
+function game_restart() 
+  snakeX, snakeY = 15, 15
+  dirX, dirY = 0, 0
+  tail = {}
+  up = false; down = false; left = false; right = false;
+  tail_length = 0
+  state = GameStates.running
+  add_food()
+end
---
+++ sourcecode/main.lua
@@ -0,0 +1,85 @@
+require('game')
+
+function love.load()
+  interval = 2 -- Sets the speed of the snake
+  add_food() -- Puts a piece of food on the screen
+  
+  -- Load Game Soundtrack
+  music = love.audio.newSource('soundtrack.mp3', 'static') -- Load sound track
+  music:setLooping(true) -- Keep the music looped
+  music:play() -- Play music
+end
+
+-- Draw the Game
+function love.draw()
+	game_draw()
+  
+  -- Draw GameOver Text
+  if state == GameStates.game_over then
+    love.graphics.print("Game Over!", 500, 300, 0, 4, 4)
+    love.graphics.print("Press A or B to restart", 430, 400, 0, 3, 3)
+  end
+  
+  -- Draw Pause Text
+  if state == GameStates.pause then
+    love.graphics.print("Pause", 550, 300, 0, 4, 4)
+    love.graphics.print("Press A to continue", 450, 400, 0, 3, 3)
+  end
+end
+
+-- Update the Game
+function love.update()
+  if state == GameStates.running then
+    interval = interval - 1
+    if interval < 0 then
+      game_update()
+      interval = 2
+    end
+  end
+end
+
+-- Control functions for Nintendo Switch
+function love.gamepadpressed(joystick, button)
+  if button == 'dpleft' and state == GameStates.running then
+    left = true; right = false; up = false; down = false;
+  elseif button == 'dpright' and state == GameStates.running then
+    left = false; right = true; up = false; down = false;
+  elseif button == 'dpup' and state == GameStates.running then
+    left = false; right = false; up = true; down = false;
+  elseif button == 'dpdown' and state == GameStates.running then
+    left = false; right = false; up = false; down = true;
+  elseif button == 'b' and state == GameStates.game_over then
+    game_restart()
+  elseif button == 'a' and state then
+    if state == GameStates.running then
+      state = GameStates.pause
+    elseif state == GameStates.game_over then
+      game_restart()
+    else
+      state = GameStates.running
+    end
+  end
+end
+
+-- Control functions for PC for testing purposes
+function love.keypressed(key)
+  if key == 'left' and state == GameStates.running then
+    left = true; right = false; up = false; down = false;
+  elseif key == 'right' and state == GameStates.running then
+    left = false; right = true; up = false; down = false;
+  elseif key == 'up' and state == GameStates.running then
+    left = false; right = false; up = true; down = false;
+  elseif key == 'down' and state == GameStates.running then
+    left = false; right = false; up = false; down = true;
+  elseif key == 'r' and state == GameStates.game_over then
+    game_restart()
+  elseif key == 'p' then
+    if state == GameStates.running then
+      state = GameStates.pause
+    elseif state == GameStates.game_over then
+      game_restart()
+    else
+      state = GameStates.running
+    end
+  end
+end
